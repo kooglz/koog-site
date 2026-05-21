9325fa839df540366b079a848795038f76cf74ba
kooglz <lingzhengk@gmail.com>
2026-05-21 13:03:03 +0800

    Update content via CMS (2026-05-21 13:03)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7b6a58a6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cmvqikntch4"
+    "revision": "0.esc5uafa3is"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
